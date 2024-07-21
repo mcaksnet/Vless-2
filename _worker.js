ee638f46d568d9b2f77f3597e6067789ab25e756
mcaksnet <mastercall108@gmail.com>
2024-07-21 07:03:24 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'afdd9c4c-3831-4010-b56b-2db442334852';
 
 const พร็อกซีไอพีs = ['139.162.7.165'];
 
